4c9f0af32ea93a0c250565827ccace1419cd23f5
‘443n0511’ <>
2019-04-26 15:01:07 +0900

    index.html ボタン部分をsubmitからbuttonへ変更

--- zodiac/zodiac.js
+++ zodiac/zodiac.js
@@ -2,7 +2,7 @@ function clickBtn(){
 
     let m = document.getElementById("month").value;
     let d = document.getElementById("day").value;
-    console.log(m);
-    console.log(d);
+    console.log(m + '月'+ d +'日');
 };
+
 
