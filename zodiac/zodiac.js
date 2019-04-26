651349ca04604900da126b535e057211f613ffeb
‘443n0511’ <>
2019-04-26 22:29:56 +0900

    インデント調整

--- zodiac/zodiac.js
+++ zodiac/zodiac.js
@@ -1,11 +1,11 @@
-function clickBtn(){
+function clickBtn() {
     let y = document.getElementById("year").value;
     let m = document.getElementById("month").value;
     let d = document.getElementById("day").value;
-    
-   let date =new Date(y,m-1,d);//月は「０」を起点とするので−１をする
 
-   console.log(date);
+    let date = new Date(y, m - 1, d);//月は「０」を起点とするので−１をする
+
+      console.log(date);
 
 };
 
