64e4d897a435c058a4b3a71d3735e1a62b502ead
‘443n0511’ <>
2019-05-02 06:31:10 +0900

    webpackがつかえるようにならない…。

--- zodiac/zodiac.js
+++ zodiac/zodiac.js
@@ -1,14 +1,21 @@
+const moment = require('moment');
+require("moment-range");
+
 function clickBtn() {
     let y = document.getElementById("year").value;
     let m = document.getElementById("month").value;
     let d = document.getElementById("day").value;
 
     let date = new Date(y, m - 1, d);//月は「０」を起点とするので−１をする
+    moment('date').daysInMonth(); 
+    console.log(date);
 
-      console.log(date);
+  
+
+
+
 
 };
-
 
 
 
