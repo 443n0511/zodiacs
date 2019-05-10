15fe82f860f1a708d7da76a663426c2e16ee0a39
443n0511 <>
2019-05-10 15:05:18 +0900

    セレクトボックスから、テキストボックスへ変更

--- myproject/src/index.js
+++ myproject/src/index.js
@@ -2,6 +2,11 @@
 const moment = require('moment');
 require("moment-range");
 
+// 星座リスト
+let zodiac = new Array('山羊座', '水瓶座', '魚座', '牡羊座', '牡牛座', '双子座', '蟹座', '獅子座', '乙女座', '天秤座', '蠍座', '射手座','TENGA座','らく座');
+// 境界となる日付
+let borderDays = new Array(20, 19, 21, 20, 21, 22, 23, 23, 23, 24, 23, 22);
+
 const button = document.querySelector('.button');
 
 //[取得したい要素].addEventListner("何がきっかけか", 処理させたい関数名 );
@@ -16,10 +21,15 @@ function onClickButton() {
     document.querySelector('#log').innerHTML = 'あなたの生年月日は'+ y + '年'+ m + '月' + d + '日です。';
     
 
+
+   
     //月は「０」を起点とするので−１をする
     //toLocaleDateString()でyyyy/mm/ddの表示をさせる
-    const date = new Date(y, m - 1, d).toLocaleDateString()
-    moment('2020-01-01').isLeapYear(); // true
+    let date = new Date(y, m - 1, d).toLocaleDateString();
+    
+
+   moment('2020-01-01').isLeapYear(); // true
+   // moment('2020-01-01').isBetween('2020-10-19', '2020-10-25');
+
     console.log(date);
 };
-
