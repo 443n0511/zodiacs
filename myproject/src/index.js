ad30c064787e4e4f75b8be8ad3b79a32e0849e40
443n0511 <>
2019-05-10 21:40:32 +0900

    セレクトボックスコメントアウト削除

--- myproject/src/index.js
+++ myproject/src/index.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 require("moment-range");
 
 // 星座リスト
-let zodiac = new Array('山羊座', '水瓶座', '魚座', '牡羊座', '牡牛座', '双子座', '蟹座', '獅子座', '乙女座', '天秤座', '蠍座', '射手座','TENGA座','らく座');
+let zodiac = new Array('山羊座', '水瓶座', '魚座', '牡羊座', '牡牛座', '双子座', '蟹座', '獅子座', '乙女座', '天秤座', '蠍座', '射手座',);
 // 境界となる日付
 let borderDays = new Array(20, 19, 21, 20, 21, 22, 23, 23, 23, 24, 23, 22);
 
@@ -19,13 +19,13 @@ function onClickButton() {
 
 　  //取得して来たセレクトボックスの数値をHTMLに表示
     document.querySelector('#log').innerHTML = 'あなたの生年月日は'+ y + '年'+ m + '月' + d + '日です。';
-    
 
-
+   
    
     //月は「０」を起点とするので−１をする
     //toLocaleDateString()でyyyy/mm/ddの表示をさせる
     let date = new Date(y, m - 1, d).toLocaleDateString();
+    
     
 
    moment('2020-01-01').isLeapYear(); // true
